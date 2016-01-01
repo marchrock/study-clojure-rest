1d58cd5ec500ed385abed1e8942e7db4ad1d87c0
MarchRock <MarchRock@users.noreply.github.com>
2016-01-01 19:34:31 +0900

    Added Environ for environmental variable based config

--- src/study_clojure_rest/server.clj
+++ src/study_clojure_rest/server.clj
@@ -1,11 +1,15 @@
 (ns study-clojure-rest.server
-  (:require [org.httpkit.server :refer [run-server]]))
+  (:require [org.httpkit.server :refer [run-server]]
+            [environ.core :refer [env]]))
 
 (defonce server (atom nil))
 
+(def server-port-number
+  (env :server-port-number))
+
 (defn start-server
   [handler]
-  (if (nil? @server) (reset! server (run-server handler {:port 8080}))))
+  (if (nil? @server) (reset! server (run-server handler {:port server-port-number}))))
 
 (defn stop-server
   []
