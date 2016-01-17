efd01911535062ea6b35b0257ee8883dfedcc86f
MarchRock <MarchRock@users.noreply.github.com>
2016-01-18 00:43:56 +0900

    added ring-defaults and ring-json middleware and wrapped my default router

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-defaults "0.1.5"]
                  [http-kit "2.1.19"]
                  [compojure "1.4.0"]
                  [environ "1.0.1"]
--- src/study_clojure_rest/core.clj
+++ src/study_clojure_rest/core.clj
@@ -1,7 +1,13 @@
 (ns study-clojure-rest.core
   (:require [study-clojure-rest.server :refer [start-server]]
-            [study-clojure-rest.routing :refer [routing]]))
+            [study-clojure-rest.routing :refer [routing]]
+            [ring.middleware.defaults :as ring-default]
+            [ring.middleware.json :as ring-json]))
+
+(def app
+  (-> (ring-default/wrap-defaults routing ring-default/api-defaults)
+      (ring-json/wrap-json-body {:keywords? true})))
 
 (defn -main
   [& args]
-  (study-clojure-rest.server/start-server routing))
+  (study-clojure-rest.server/start-server app))
