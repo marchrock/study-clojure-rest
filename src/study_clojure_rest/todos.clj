53cbb3d757352d6c5329136caf6f3e714509beb7
MarchRock <MarchRock@users.noreply.github.com>
2016-01-18 23:36:08 +0900

    implemented POST method handler to insert data to db

--- src/study_clojure_rest/todos.clj
+++ src/study_clojure_rest/todos.clj
@@ -1,6 +1,19 @@
 (ns study-clojure-rest.todos
-  (:require [compojure.core :refer [routes GET POST]]
+  (:require [study-clojure-rest.database :refer [db-insert]]
+            [compojure.core :refer [routes GET POST]]
             [ring.util.response :refer [response header]]))
+
+(def todo-db-collection-name
+  "Todos")
+
+(defn has-todo-name?
+  [todo-map]
+  (contains? todo-map :name))
+
+(defn is-todo?
+  [probably-todo-map]
+  (do (println probably-todo-map)
+      (has-todo-name? probably-todo-map)))
 
 (defn get-all-todos
   [req]
@@ -8,7 +21,13 @@
       (header "Content-Type" "application/json")))
 
 (defn add-new-todos
-  [req])
+  [req]
+  (let [body (req :body)]
+    (if (is-todo? body) (do (db-insert todo-db-collection-name body)
+                            (-> (response (assoc {} :result "success" :value body))
+                                (header "Content-Type" "application/json; charset=utf-8")))
+                        (-> (response {:error "invalid todo data"})
+                            (header "Content-Type" "application/json; charset=utf-8")))))
 
 (defn todos-routes
   []
